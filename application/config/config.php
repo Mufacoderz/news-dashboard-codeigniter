6cc9780e6487131bbee89f51e91a2305c83e5f84
Muhammad <mf9555759@gmail.com>
2025-11-15 18:36:50 +0800

    secret key

--- application/config/config.php
+++ application/config/config.php
@@ -532,3 +532,9 @@ $config['rewrite_short_tags'] = FALSE;
 | Array:		array('10.0.1.200', '192.168.5.0/24')
 */
 $config['proxy_ips'] = '';
+
+
+
+
+
+$config['google_maps_key'] = 'AIzaSyB55Np3_WsZwUQ9NS7DP-HnneleZLYZDNw&amp';
